2565bc2bb63a8625921942239c4432dce4ff5389
KenAspeling <ken.aspeling@gmail.com>
2025-09-24 00:20:33 +0200

    Added 3 Pages
    Page 1 "Home Page" has the date, the other 2 still empty.
    
    Also made it look prettier

--- streaks_app/lib/main.dart
+++ streaks_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,9 +15,9 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'Streaks App',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
         ),
         home: HomePage(),
       ),
@@ -34,47 +35,158 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = MainPage();
+        break;
+      case 1:
+        page = ManagePage();
+        break;
+      case 2:
+        page = AddPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+    
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
       body: Row(
+        
         children: [
           SafeArea(
-            child: NavigationRail(
-              destinations: [
-                NavigationRailDestination(
-                  icon: Icon(Icons.home), 
-                  label: Text("Home")),
-                NavigationRailDestination(
-                  icon: Icon(Icons.manage_search), 
-                  label: Text("Manage")),
-                NavigationRailDestination(
-                  icon: Icon(Icons.add), 
-                  label: Text("Add")),
-              ],
-              selectedIndex: 0,
-              onDestinationSelected: (value) {
-                print('selected: $value');
-              },
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width > 600 ? 175 : null,
+              child: NavigationRail(
+                unselectedIconTheme: IconThemeData(color: Theme.of(context).highlightColor),
+                selectedIconTheme: IconThemeData(color: Theme.of(context).primaryColor),
+                selectedLabelTextStyle: TextStyle(color: Theme.of(context).canvasColor),
+                unselectedLabelTextStyle: TextStyle(color: Theme.of(context).highlightColor),
+                indicatorColor: Theme.of(context).canvasColor,
+                backgroundColor: Theme.of(context).primaryColor,
+                extended: MediaQuery.of(context).size.width > 600,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home), 
+                    label: Text("Home")),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.manage_search), 
+                    label: Text("Manage")),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.add), 
+                    label: Text("Add")),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
             ) 
           ),
           Expanded(
             child: Container(
               color: Theme.of(context).colorScheme.primaryContainer,
-              child: MainPage()
+              child: page,
             )
           )
         ],
       ),
     );
+    });
   }
 }
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            TimeShow()
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class TimeShow extends StatefulWidget{
+  @override
+  State<TimeShow> createState() => _TimeShowState();
+}
+
+class _TimeShowState extends State<TimeShow> {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displaySmall!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    final today = DateTime.now();
+    final formattedDate = DateFormat('EEEE, MMMM dd').format(today);
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          formattedDate, 
+          style: style,),
+      ),
+    );
+  }
+}
+
+class ManagePage extends StatefulWidget {
+  @override
+  State<ManagePage> createState() => _ManagePageState();
+}
+
+class _ManagePageState extends State<ManagePage> {
   @override
   Widget build(BuildContext context) {
 
     return Center(
+      child: Scaffold(
+        body: Column(
+          children: [Text("Manage Page")],
+        ),
+      ),
+    );
+  }
+}
+
+class AddPage extends StatefulWidget {
+  @override
+  State<AddPage> createState() => _AddPageState();
+}
+
+class _AddPageState extends State<AddPage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(
+      child: Scaffold(
+        body: Column(
+          children: [Text("Add Page")],
+        ),
+      ),
     );
   }
 }
